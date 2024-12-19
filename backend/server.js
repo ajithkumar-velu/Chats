803bdf7dc853d22c2c6a67ffad94e01182db4f96
Ajith Kumar V <ajithajk46@gmail.com>
2024-12-19 17:35:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,35 @@
+import express from "express"
+import {config} from 'dotenv'
+import connectDB from "./lib/db.js"
+import authRouter from "./routes/authRouter.js"
+import cookieParser from "cookie-parser"
+import cors from 'cors'
+import messageRouter from "./routes/MessageRouter.js"
+import { app, io, server } from "./lib/socket.js"
 
-import express from "express";
-import { config } from "dotenv";
-import connectDB from "./lib/db.js";
-import authRouter from "./routes/authRouter.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import messageRouter from "./routes/MessageRouter.js";
-import { app, io, server } from "./lib/socket.js";
+config()
+const PORT = process.env.PORT || 2000
 
-config();
-const PORT = process.env.PORT || 2000;
 
-app.use(express.json());
-app.use(cookieParser());
 
-// CORS configuration
+app.use(express.json())
+app.use(cookieParser())
 app.use(cors({
-    origin: "https://chats-frontend-xi.vercel.app",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
+     origin: "https://chats-frontend-xi.vercel.app",
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true,}
+))
 
-// Handle preflight requests
-app.options('*', cors({
-    origin: "https://chats-frontend-xi.vercel.app",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
 
-// Routes
-app.use('/api/user', authRouter);
-app.use('/api/message', messageRouter);
+app.use('/api/user', authRouter)
+app.use("/api/message", messageRouter)
 
-app.get("/", (req, res) => {
-    res.send("It's working");
-});
+app.get("/", (req, res)=>{
+    res.send("It's working")
+})
 
-// Start server
-server.listen(PORT, () => {
+server.listen(PORT, ()=>{
     console.log(`Server running on PORT: ${PORT}`);
-    connectDB();
-});
+    connectDB()
+})
+
