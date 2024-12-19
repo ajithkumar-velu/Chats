91073fbd2125d6848ea1e59ac439b13d946703b7
Ajith Kumar V <ajithajk46@gmail.com>
2024-12-19 17:37:46 +0530

    Update socket.js

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { createServer } from 'http'
 import { Server } from "socket.io"
+import cors from "cors"
 
 const app = express()
 const server = createServer(app)
