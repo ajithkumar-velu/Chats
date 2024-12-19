5cdaca2bda6507a380bb1872626bbf46fd86b749
Ajith Kumar V <ajithajk46@gmail.com>
2024-12-19 15:51:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,7 @@ const PORT = process.env.PORT || 2000
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: "https://chats-frontend-virid.vercel.app",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}))
+app.use(cors())
 
 app.use('/api/user', authRouter)
 app.use("/api/message", messageRouter)
