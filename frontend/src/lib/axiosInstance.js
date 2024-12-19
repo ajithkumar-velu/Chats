d3b332dc50da60966dd2d9f2bd0efb0e66267cc6
Ajith Kumar V <ajithajk46@gmail.com>
2024-12-19 19:44:40 +0530

    Update axiosInstance.js

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstanace = axios.create({
-    baseURL: "https://chats-backend-beige.vercel.app"+ "/api",
+    baseURL: "https://chats-backend2.vercel.app"+ "/api",
     withCredentials: true,
      headers: {
         "Content-Type": "application/json",
