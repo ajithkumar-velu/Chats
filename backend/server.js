5d47a94134a071c56b60fe6b39d7147b3d930916
Ajith Kumar V <ajithajk46@gmail.com>
2024-12-19 19:22:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,15 @@ const PORT = process.env.PORT || 2000
 
 
 
-app.use(express.json())
-app.use(cookieParser())
 app.use(cors({
     origin: ["https://chats-frontend-xi.vercel.app"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
+app.use(express.json())
+
+app.use(cookieParser())
+
 
 app.use('/api/user', authRouter)
 app.use("/api/message", messageRouter)
